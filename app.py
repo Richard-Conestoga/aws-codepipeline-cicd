f3255791dfc07448ce2513afc9fad5654a0ba558
rbiscazzi <code@laptop.simplelogin.com>
2025-12-01 01:40:09 -0500

    Trying another approach

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from cicd_cdk_aws_codepipeline.infra_stack import Rb8903530InfraStack
+# from cicd_cdk_aws_codepipeline.infra_stack import Rb8903530InfraStack
 from cicd_cdk_aws_codepipeline.pipeline_stack import Rb8903530PipelineStack
 
 app = cdk.App()
 
-infra = Rb8903530InfraStack(app, "Rb8903530InfraStack")
+# Optional direct deploy of infra stack
+# Rb8903530InfraStack(app, "Rb8903530InfraStack")
 
 Rb8903530PipelineStack(
     app,
@@ -13,7 +14,7 @@ Rb8903530PipelineStack(
     repo_owner="Richard-Conestoga",
     repo_name="aws-codepipeline-cicd",
     branch="main",
-    connection_arn="arn:aws:codestar-connections:us-east-1:349070XXXXXX:connection/XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+    connection_arn="arn:aws:codeconnections:us-east-1:349070093011:connection/fe767164-5b41-43b1-98c5-6d9fee3d6cda",
 )
 
 app.synth()
--- cicd_cdk_aws_codepipeline/pipeline_stack.py
+++ cicd_cdk_aws_codepipeline/pipeline_stack.py
@@ -1,12 +1,13 @@
-from aws_cdk import (
-    Stack,
-    aws_codepipeline as codepipeline,
-    aws_codepipeline_actions as cp_actions,
-    aws_codebuild as codebuild,
-    aws_iam as iam,
-)
+from aws_cdk import Stack, Stage
+from aws_cdk import pipelines
 from constructs import Construct
 
+from cicd_cdk_aws_codepipeline.infra_stack import Rb8903530InfraStack
+
+class Rb8903530AppStage(Stage):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        Rb8903530InfraStack(self, "Rb8903530InfraStack")
 
 class Rb8903530PipelineStack(Stack):
     def __init__(
@@ -22,80 +23,27 @@ class Rb8903530PipelineStack(Stack):
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        source_output = codepipeline.Artifact()
-
-        pipeline = codepipeline.Pipeline(
+        pipeline = pipelines.CodePipeline(
             self,
-            "Rb8903530Pipeline",
+            "Rb8903530CdkPipeline",
             pipeline_name="rb8903530-cdk-pipeline",
-        )
-
-        pipeline.role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AWSCodePipeline_FullAccess")
-        )
-
-        source_action = cp_actions.CodeStarConnectionsSourceAction(
-            action_name="GitHub_Source",
-            owner=repo_owner,
-            repo=repo_name,
-            branch=branch,
-            connection_arn=connection_arn,
-            output=source_output,
-        )
-
-        pipeline.add_stage(
-            stage_name="Source",
-            actions=[source_action],
-        )
-
-        codebuild_role = iam.Role(
-            self,
-            "Rb8903530CodeBuildRole",
-            role_name="rb8903530-codebuild-role",
-            assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com"),
-        )
-
-        codebuild_role.add_managed_policy(
-            iam.ManagedPolicy.from_aws_managed_policy_name("AdministratorAccess")
-        )
-
-        project = codebuild.PipelineProject(
-            self,
-            "Rb8903530CodeBuild",
-            project_name="rb8903530-cdk-build",
-            role=codebuild_role,
-            environment=codebuild.BuildEnvironment(
-                build_image=codebuild.LinuxBuildImage.STANDARD_7_0,
-            ),
-            build_spec=codebuild.BuildSpec.from_object(
-                {
-                    "version": "0.2",
-                    "phases": {
-                        "install": {
-                            "commands": [
-                                "python -m pip install --upgrade pip",
-                                "pip install -r requirements.txt",
-                                "npm install -g aws-cdk",
-                            ]
-                        },
-                        "build": {
-                            "commands": [
-                                "cdk synth",
-                                "cdk deploy Rb8903530InfraStack --require-approval never",
-                            ]
-                        },
-                    },
-                }
+            synth=pipelines.CodeBuildStep(
+                "Synth",
+                input=pipelines.CodePipelineSource.connection(
+                    repo_string=f"{repo_owner}/{repo_name}",
+                    branch=branch,
+                    connection_arn=connection_arn,
+                ),
+                install_commands=[
+                    "python -m pip install --upgrade pip",
+                    "pip install -r requirements.txt",
+                    "npm install -g aws-cdk",
+                ],
+                commands=[
+                    "cdk synth",
+                ],
             ),
         )
 
-        build_action = cp_actions.CodeBuildAction(
-            action_name="CDK_Build_Deploy",
-            project=project,
-            input=source_output,
-        )
+        pipeline.add_stage(Rb8903530AppStage(self, "Rb8903530App"))
 
-        pipeline.add_stage(
-            stage_name="BuildAndDeploy",
-            actions=[build_action],
-        )
